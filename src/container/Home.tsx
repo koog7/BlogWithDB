63ff41306c8aaa9e0071b9f1289b41236ee6852f
koog7 <k.ognev709@mail.com>
2024-06-27 12:37:32 +0600

    Добавлена функция запроса для получения ключа и значений ключа объекта , а так же добавлено отображения этих данных на странице

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,8 +1,45 @@
+import axiosApi from "../axios/AxiosAPI.tsx";
+import {useEffect, useState} from "react";
+import {Card, CardContent, Container, Typography} from "@mui/material";
+interface Post {
+    editorContent: string;
+    title: string;
+}
+
+interface PostsResponse {
+    [key: string]: Post;
+}
+
 const Home = () => {
+    const [dataDB , setDataDB] = useState<PostsResponse >()
+
+    useEffect(() => {
+        axiosApi.get<PostsResponse >('/posts.json')
+            .then(response => {
+                setDataDB(response.data);
+            })
+    }, []);
+
+    console.log(dataDB)
     return (
-        <div>
-            home
-        </div>
+        <Container>
+                {dataDB ? (
+                    Object.entries(dataDB).map(([key, post]) => (
+                        <Card key={key} sx={{ maxWidth: 345, borderRadius: '10px', marginTop: '20px'}}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {post.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" dangerouslySetInnerHTML={{ __html: post.editorContent }} />
+                            </CardContent>
+                        </Card>
+                    ))
+                ) : (
+                    <Typography variant="h6" component="p">
+                        There is nothing...
+                    </Typography>
+                )}
+        </Container>
     );
 };
 
