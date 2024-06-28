7e95d811cfab768f5722b10ef4f48627602df7c1
koog7 <k.ognev709@mail.com>
2024-06-28 16:15:49 +0600

    Помимо ГГ/ММ/ДД теперь отображается ЧЧ/ММ.ССÐ

--- src/components/FormSendler.tsx
+++ src/components/FormSendler.tsx
@@ -43,7 +43,13 @@ const FormSendler = () => {
 
     const onFormSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-        const currentDate = new Date().toLocaleDateString();
+        const d = new Date();
+        const currentDate = d.getFullYear() + '-' +
+        ('0' + (d.getMonth() + 1)).slice(-2) + '-' +
+        ('0' + d.getDate()).slice(-2) + ' ' +
+        ('0' + d.getHours()).slice(-2) + ':' +
+        ('0' + d.getMinutes()).slice(-2) + ':' +
+        ('0' + d.getSeconds()).slice(-2);
 
         if (!PostData) return;
 
